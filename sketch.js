23d38fa0bd20fbcbb24e34eeae6a191e97258060
Camila Machado de Araújo <57112941+camila-machado@users.noreply.github.com>
2022-06-04 14:42:35 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,24 +1,31 @@
 const Engine = Matter.Engine;
+const Render = Matter.Render;
 const World = Matter.World;
 const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+const Body = Matter.Body;
+const Composites = Matter.Composites;
+const Composite = Matter.Composite;
 
-function preload()
-{
-}
+var engine;
+var world;
 
 function setup() 
 {
-  createCanvas(600,700);
+  createCanvas(500,700);
+  engine = Engine.create();
+  world = engine.world;
+  
+  //configurações para rect() e ellipse() e texto
+  rectMode(CENTER);
+  ellipseMode(RADIUS);
+  textSize(50)
+  
 }
 
 function draw() 
 {
   background(51);
- 
- 
- 
-  Engine.update(engine);
-  drawSprites();
+  Engine.update(engine); 
 }
-
 
